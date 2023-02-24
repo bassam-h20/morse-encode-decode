7ba7a69bdef5033d5adadd36fe68b3ab5aa886d0
Bassam Ali (Student) <bassam2.ali@live.uwe.ac.uk@csctcloud.prxhn32zsyjupl12zde3wlfkch.cwx.internal.cloudapp.net>
2023-02-24 15:39:47 +0000

    updating morse.py

--- morse.py
+++ morse.py
@@ -1,139 +1,210 @@
-class morse_tree:
+class morse:
     def __init__(self,data):
         self.left = None
         self.right = None
         self.data = data
-    def encode():
-        print("Not complete")
-    def decode():
-            print("not complete")
+    def encode(self, word):
+        return 0
+    def decode(self, morse_code):
+        return 0
 
 
-root = morse_tree('start')
+root = morse('start')
 
 
 #Left side (E)
-root.left = morse_tree('E')
+root.left = morse('E')
 
 #Left side (E) - First level - (Under E)
-root.left.left = morse_tree('I')
-root.left.right = morse_tree('A')
+root.left.left = morse('I')
+root.left.right = morse('A')
 
 #Left side (E) - Second level - (Under I)
-root.left.left.left = morse_tree('S')
-root.left.left.right = morse_tree('U')
+root.left.left.left = morse('S')
+root.left.left.right = morse('U')
 
 #Left side (E) - Second level - (under A)
-root.left.right.left = morse_tree('R')
-root.left.right.right = morse_tree('W')
+root.left.right.left = morse('R')
+root.left.right.right = morse('W')
 
 #Left side (E) - Third level - (Under S)
-root.left.left.left.left = morse_tree('H')
-root.left.left.left.right = morse_tree('V')
+root.left.left.left.left = morse('H')
+root.left.left.left.right = morse('V')
 
 #Left side (E) - Third level - (Under U)
-root.left.left.right.left = morse_tree('F')
-#root.left.left.right.right = morse_tree('blank')
+root.left.left.right.left = morse('F')
+root.left.left.right.right = morse('blank')
 
 #Left side (E) - Third level - (Under R)
-root.left.right.left.left = morse_tree('L')
-#root.left.right.left.right = morse_tree('blank')
+root.left.right.left.left = morse('L')
+root.left.right.left.right = morse('blank')
 
 #Left side (E) - Third level - (Under W)
-root.left.right.right.left = morse_tree('P')
-root.left.right.right.right = morse_tree('J')
+root.left.right.right.left = morse('P')
+root.left.right.right.right = morse('J')
 
 #Left side (E) - Fourth level - (under H)
-root.left.left.left.left.left = morse_tree('5')
-root.left.left.left.left.right = morse_tree('4')
+root.left.left.left.left.left = morse('5')
+root.left.left.left.left.right = morse('4')
 
 #Left side (E) - Fourth level - (under V)
-root.left.left.left.right.right = morse_tree('3')
-#root.left.left.left.right.left = morse_tree('blank')
+root.left.left.left.right.right = morse('3')
+root.left.left.left.right.left = morse('blank')
 
 #Left side (E) - Fourth level - (under F)
-#root.left.left.right.left.left = morse_tree('blank')
-#root.left.left.right.left.right = morse_tree('blank')
+root.left.left.right.left.left = morse('blank')
+root.left.left.right.left.right = morse('blank')
 
 #Left side (E) - Fourth level - (under blank)
-#root.left.left.right.right.left = morse_tree('blank')
-root.left.left.right.right.right = morse_tree('2')
+root.left.left.right.right.left = morse('blank')
+root.left.left.right.right.right = morse('2')
 
 #Left side (E) - Fourth level - (Under L)
-#root.left.right.left.left.left = morse_tree('blank')
-#root.left.right.left.left.right = morse_tree('blank')
+root.left.right.left.left.left = morse('blank')
+root.left.right.left.left.right = morse('blank')
 
 #Left side (E) - Fourth level - (Under blank)
-root.left.right.left.right.left = morse_tree('+')
-#root.left.right.left.right.right = morse_tree('blank')
+root.left.right.left.right.left = morse('+')
+root.left.right.left.right.right = morse('blank')
 
 #Left side (E) - Fourth level - (Under P)
-#root.left.right.right.left.left = morse_tree('blank')
-#root.left.right.right.left.right = morse_tree('blank')
+root.left.right.right.left.left = morse('blank')
+root.left.right.right.left.right = morse('blank')
 
 #Left side (E) - Fourth level - (Under J)
-root.left.right.right.right.left = morse_tree('blank')
-root.left.right.right.right.right = morse_tree('1')
+root.left.right.right.right.left = morse('blank')
+root.left.right.right.right.right = morse('1')
 
 #Right side (T)
-root.right = morse_tree('T')
+root.right = morse('T')
 
 #Right side (T) - First level - (under T)
-root.right.left = morse_tree('N')
-root.right.right = morse_tree('M')
+root.right.left = morse('N')
+root.right.right = morse('M')
 
 #Right side (T) - Second level - (under N)
-root.right.left.left = morse_tree('D')
-root.right.left.right = morse_tree('K')
+root.right.left.left = morse('D')
+root.right.left.right = morse('K')
 
 #Right side (T) - Second level - (under M)
-root.right.right.left = morse_tree('G')
-root.right.right.right = morse_tree('O')
+root.right.right.left = morse('G')
+root.right.right.right = morse('O')
 
 #Right side (T) - Third level - (under D)
-root.right.left.left.left = morse_tree('B')
-root.right.left.left.right = morse_tree('X')
+root.right.left.left.left = morse('B')
+root.right.left.left.right = morse('X')
 
 #Right side (T) - Third level - (under K)
-root.right.left.right.left = morse_tree('C')
-root.right.left.right.right = morse_tree('Y')
+root.right.left.right.left = morse('C')
+root.right.left.right.right = morse('Y')
 
 #Right side (T) - Third level - (under G)
-root.right.right.left.left = morse_tree('Z')
-root.right.right.left.right = morse_tree('Q')
+root.right.right.left.left = morse('Z')
+root.right.right.left.right = morse('Q')
 
 #Right side (T) - Third level - (under O)
-#root.right.right.right.left = morse_tree('blank')
-#root.right.right.right.right = morse_tree('blank')
+root.right.right.right.left = morse('blank')
+root.right.right.right.right = morse('blank')
 
 #Right side (T) - Fourth level - (under B)
-root.right.left.left.left.left = morse_tree('6')
-root.right.left.left.left.right = morse_tree('=')
+root.right.left.left.left.left = morse('6')
+root.right.left.left.left.right = morse('=')
 
 #Right side (T) - Fourth level - (under X)
-root.right.left.left.right.left = morse_tree('/')
-#root.right.left.left.right.right = morse_tree('blank')
+root.right.left.left.right.left = morse('/')
+root.right.left.left.right.right = morse('blank')
 
 #Right side (T) - Fourth level - (under C)
-#root.right.left.right.left.left = morse_tree('blank')
-#root.right.left.right.left.right = morse_tree('blank')
+root.right.left.right.left.left = morse('blank')
+root.right.left.right.left.right = morse('blank')
 
 #Right side (T) - Fourth level - (under Y)
-#root.right.left.right.right.left = morse_tree('blank')
-#root.right.left.right.right.right = morse_tree('blank')
+root.right.left.right.right.left = morse('blank')
+root.right.left.right.right.right = morse('blank')
 
 #Right side (T) - Fourth level - (under Z)
-root.right.right.left.left.left = morse_tree('7')
-#root.right.right.left.left.right = morse_tree('blank')
+root.right.right.left.left.left = morse('7')
+root.right.right.left.left.right = morse('blank')
 
 #Right side (T) - Fourth level - (under Q)
-#root.right.right.left.right.left = morse_tree('blank')
-#root.right.right.left.right.right = morse_tree('blank')
+root.right.right.left.right.left = morse('blank')
+root.right.right.left.right.right = morse('blank')
 
 #Right side (T) - Fourth level - (under left blank under O)
-root.right.right.right.left.left = morse_tree('8')
-#root.right.right.right.left.right = morse_tree('blank')
+root.right.right.right.left.left = morse('8')
+root.right.right.right.left.right = morse('blank')
 
 #Right side (T) - Fourth level - (under right blank under O)
-root.right.right.right.right.left = morse_tree('9')
-root.right.right.right.right.right = morse_tree('0')
+root.right.right.right.right.left = morse('9')
+root.right.right.right.right.right = morse('0')
+
+
+morse_dict = {
+#First level
+"E" : ".",
+"T" : "-",
+#Second level
+"I" : "..",
+"A" : ".-",
+"N" : "-.",
+"M" : "--",
+#Third level
+"S" : "...",
+"U" : "..-",
+"R" : ".-.",
+"W" : ".--",
+"D" : "-..",
+"K" : "-.-",
+"G" : "--.",
+"O" : "---",
+#Fourth Level
+"H" : "....",
+"V" : "...-",
+"F" : "..-.",
+"" : "..--",
+"L": ".-..",
+"" : ".-.-",
+"P" : ".--.",
+"J" : ".---",
+"B" : "-...",
+"X" : "-..-",
+"C" : "-.-.",
+"Y" : "-.--",
+"Z" : "--..",
+"Q" : "--.-",
+"" : "---.",
+"" : "----",
+#Fifth Level
+"5" : ".....",
+"4" : "....-",
+"" : "...-.",
+"3" : "...--",
+"" : "..-..",
+"" : "..-.-",
+"" : "..--.",
+"2" : "..---",
+"" : ".-...",
+"" : ".-..-",
+"+" : ".-.-.",
+"" : ".-.--",
+"" : ".--..",
+"" : ".--.-",
+"" : ".---.",
+"1" : ".----",
+"6" : "-....",
+"=" : "-...-",
+"/" : "-..-.",
+"" : "-..--",
+"" : "-.-..",
+"" : "-.-.-",
+"" : "-.--.",
+"" : "-.---",
+"7" : "--...",
+"" : "--..-",
+"" : "--.-.",
+"" : "--.--",
+"8" : "---..",
+"" : "---.-",
+"9" : "----.",
+"0" : "-----"
+}
