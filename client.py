7d38421a5b75dd4942838870fd20029b77c9ac10
Bassam Ali (Student) <bassam2.ali@live.uwe.ac.uk@csctcloud.prxhn32zsyjupl12zde3wlfkch.cwx.internal.cloudapp.net>
2023-04-08 01:35:27 +0100

    updating client.pyn

--- client.py
+++ client.py
@@ -8,7 +8,10 @@ from morse import morse
 async def recv_message(websocket):
     message = json.loads(await websocket.recv())
     return message
-    
+
+async def recv_message2(websocket):
+    message = json.loads(await websocket.recv())
+    return message
     
 async def send_echo(websocket, sender: str, msg: str, client_id) -> str:
     encoded_msg = morse.encode_ham(sender, 'echo', msg)
@@ -18,8 +21,7 @@ async def send_echo(websocket, sender: str, msg: str, client_id) -> str:
 async def send_time(websocket, sender: str, client_id) -> str:
     encoded_msg = morse.encode_ham(sender, "time", 'hello world')
     outward_message = {'type':'morse_evt','client_id':client_id, 'payload': encoded_msg}
-    
-    await websocket.send(json.dumps(encoded_msg))
+    await websocket.send(json.dumps(outward_message))
 
 
 async def main():
@@ -36,21 +38,21 @@ async def main():
             print('Did not receive a correct join message')
             return 0
 
-
+        print("Echo:")
         echo_sender = input("Enter sender name: ")
-        echo_message = input("Enter the message to be sent to the server: ")
+        echo_message = input("Enter the message to be sent to the server: ")        
         await send_echo(websocket, echo_sender, echo_message, client_id)
         echo_response = await recv_message(websocket)
         print("The server sent back:\n",echo_response['payload'])
         print(morse.decode_ham(echo_response['payload']))
         
-        """
+        print("\nTime:")
         time_sender = "s"
         await send_time(websocket, time_sender, client_id)
-        time_response = await recv_message(websocket)
-        print("The server sent back:")
-        print(morse.decode_ham(time_response('payload')))
-        """
+        time_response = await recv_message2(websocket)
+        print("The server sent back:\n",time_response['payload'])
+        print(morse.decode_ham(time_response['payload']))
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
